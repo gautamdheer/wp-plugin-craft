5893e859de22ec62cc3a5dd4168844872f4a0d3a
gautam dheer <gautam.litesh@gmail.com>
2024-10-27 15:17:58 +0530

    wip WP Custom Wocom Product

--- wp-content/plugins/wp-custom-wocom-product/wp-custom-wocom-product.php
+++ wp-content/plugins/wp-custom-wocom-product/wp-custom-wocom-product.php
@@ -7,3 +7,22 @@
  * Author: Gautam Dheer
  */
 
+ if(!defined("WPINC")){
+    exit;
+ }
+ 
+ // add plugin to menu
+ add_action("admin_menu","wcp_add_plugin_menu");
+
+ function wcp_add_plugin_menu(){
+    add_menu_page("Woocommerce Product Creator","Woocommerce Product Creator","manage_options","wcp-product-creator","wcp_add_woocommerce_product_layout","dashicons-cloud-upload",8);
+ }
+
+ function wcp_add_woocommerce_product_layout(){
+    echo "<h2>Woocommerce Product Creator</h2>";
+   // include_once(WCP_PLUGIN_DIR."templates/wcp-product-layout.php");
+ }
+
+
+ 
+
