39147cfb7f0360ed820c3a46d825b266b82a9e75
Gautam Dheer <gautam.litesh@gmail.com>
2024-11-01 15:44:22 +0530

    wp crud apis wip

--- wp-content/plugins/wp-crud-apis/wp-crud-apis.php
+++ wp-content/plugins/wp-crud-apis/wp-crud-apis.php
@@ -18,13 +18,16 @@ register_activation_hook(__FILE__, 'wp_crud_apis_activate');
 function wp_crud_apis_activate(){
     // create custom table  
     global $wpdb;
-    $table_name = $wpdb->prefix . 'wp_crud_apis';
+    $table_name = $wpdb->prefix . 'students';
+    $wp_collate = $wpdb->get_charset_collate();
+            
     $wpdb->query("CREATE TABLE IF NOT EXISTS $table_name (
         id INT AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL
-    )");
-}
+        email VARCHAR(255) NOT NULL,
+        phone INT NOT NULL
+    )" . $wp_collate);
+ }
 
 
 // Action hook for registration of apis
