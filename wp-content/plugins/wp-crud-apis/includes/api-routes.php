8a49c134c7ffabe6d5ff7bd2d6bee8eca727cb79
Gautam Dheer <gautam.litesh@gmail.com>
2024-11-01 17:40:22 +0530

    wp rest api plugin create and get functionality done

--- wp-content/plugins/wp-crud-apis/includes/api-routes.php
+++ wp-content/plugins/wp-crud-apis/includes/api-routes.php
@@ -1,14 +1,79 @@
 <?php
-    // create
-    register_rest_route('wp-crud-apis/v1', '/create', array(
-        'methods' => 'POST',
-        'callback' => 'create_post',
-    ));
-    
-    // read
+// create student
+register_rest_route('students/v1', 'students', [
+    'methods' => 'POST',
+    'callback' => 'wcp_handle_create_student',
+    'args' => [
+        'name' => [
+            'required' => true,
+            'type' => 'string',
+        ],
+        'email' => [
+            'required' => true,
+            'type' => 'string',
+        ],
+        'phone' => [
+            'required' => true,
+            'type' => 'string',
+        ],
+    ],
+]);
 
-    // update
+// read all students
+register_rest_route('students/v1', '/students', [
+    'methods' => 'GET',
+    'callback' => 'wcp_handle_get_students',
+]);
 
-    // delete   
+// read single student
+
+// update single student
+
+// delete single student
+
+// handle create student
+function wcp_handle_create_student($request)
+{
+    $name = $request->get_param('name');
+    $email = $request->get_param('email');
+    $phone = $request->get_param('phone');
+
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'students';
+
+    // insert data into database
+    $wpdb->insert($table_name, [
+        'name' => $name,
+        'email' => $email,
+        'phone' => $phone,
+    ]);
+
+    if ($wpdb->insert_id > 0) {
+        return rest_ensure_response([
+            'success' => true,
+            'message' => 'Student created successfully',
+            'data' => $request->get_params(),
+        ]);
+    } else {
+        return rest_ensure_response([
+            'success' => false,
+            'message' => 'Student creation failed',
+        ], 500);
+    }
+}
+
+// get all students
+function wcp_handle_get_students()
+{
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'students';
+    $students = $wpdb->get_results('SELECT * From ' . $table_name, ARRAY_A);
+
+    return rest_ensure_response([
+        'success' => true,
+        'message' => 'Students fetched successfully',
+        'data' => $students,
+    ]);
+}
 
 ?>
